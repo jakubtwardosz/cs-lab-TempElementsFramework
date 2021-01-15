c388b423e6b6f61b01d76016783e6651d4a0948f
Jakub Twardosz <jtwardosz13@gmail.com>
2021-01-15 10:57:00 +0100

    init

---
+++ TempElementsConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TempElementsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
